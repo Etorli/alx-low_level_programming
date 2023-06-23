d462e7ab8482b3746c8a59de96031ecc6b5222bf
Etorli <Etornamprecious18@gmail.com>
2023-06-22 17:54:11 -0700

    main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -14,11 +14,9 @@ int main(void)
         i = 0;
         /*while (i < 10)*/
         /*{*/
-        /*        putchar(i);*/
-	/*        i++;*/
+        /*      putchar(i);*/
+	/*      i++;*/
         /*}*/
-    
-        printf("Infinite loop avoided! \\o/\n");
-
+        printf("Infite loop avoided! \\o/\n");
         return (0);
 }
